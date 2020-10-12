7dd65707d8c25dce5a78e241d2521802058ea144
pollet <76pollet@gmail.com>
2020-10-12 10:59:45 +0300

    write unit tests

--- lib/users_api/users.ex
+++ lib/users_api/users.ex
@@ -6,12 +6,20 @@ defmodule UsersApi.Users do
   alias UsersApi.Users.User
   alias UsersApi.Repo
 
+  @doc """
+  creates a user with the given attributes
+  """
+  @spec create_user(map()) :: {:ok, %User{}} | {:error, Ecto.Changeset.t()}
   def create_user(attrs) do
     %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
   end
 
+  @doc """
+  updates the given user with the given attributes
+  """
+  @spec update_user(%User{}, map()) :: {:ok, %User{}} | {:error, Ecto.Changeset.t()}
   def update_user(user, attrs) do
     user
     |> User.changeset(attrs)
